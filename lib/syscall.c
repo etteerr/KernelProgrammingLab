2945b5496ff6ffed2eb4becc6d804e29b22ee0bc
etteerr <e.j.diepgrond@gmail.com>
2017-09-19 20:37:18 +0200

    Implemented a single sysenter entry function in user lib syscall

--- lib/syscall.c
+++ lib/syscall.c
@@ -7,6 +7,14 @@ static inline int32_t syscall(int num, int check, uint32_t a1, uint32_t a2,
         uint32_t a3, uint32_t a4, uint32_t a5)
 {
     int32_t ret;
+    
+    asm volatile(
+    "mov $1, %%ESP\n"
+    "cpuid"
+    : "=edx" (ret)
+    :
+    : "%eax", "%ebx", "%ecx"
+    );
 
     /*
      * Generic system call: pass system call number in AX,
@@ -21,17 +29,36 @@ static inline int32_t syscall(int num, int check, uint32_t a1, uint32_t a2,
      * potentially change the condition codes and arbitrary
      * memory locations.
      */
-
-    asm volatile("int %1\n"
+    
+    if (ret & 1 << 11)  {
+        asm volatile (
+        "call mini\n"
+        "mini:\n"
+        "push %%ebp\n"
+        "mov %%esp, %%ebp\n"
+        "sysenter"
         : "=a" (ret)
-        : "i" (T_SYSCALL),
-          "a" (num),
+        : "a" (num),
           "d" (a1),
           "c" (a2),
           "b" (a3),
           "D" (a4),
           "S" (a5)
-        : "cc", "memory");
+        : "cc", "memory"
+        );
+    }else{
+    
+        asm volatile("int %1\n"
+            : "=a" (ret)
+            : "i" (T_SYSCALL),
+              "a" (num),
+              "d" (a1),
+              "c" (a2),
+              "b" (a3),
+              "D" (a4),
+              "S" (a5)
+            : "cc", "memory");
+    }
 
     if(check && ret > 0)
         panic("syscall %d returned %d (> 0)", num, ret);
