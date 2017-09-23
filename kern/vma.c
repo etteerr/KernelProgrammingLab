1e27e97167f4171c697f5409c40ff38aca33c1ec
etteerr <e.j.diepgrond@gmail.com>
2017-09-23 15:40:50 +0200

    struct env to env_t

--- kern/vma.c
+++ kern/vma.c
@@ -1,19 +1,19 @@
 #include <kern/vma.h>
 
-int vma_new(struct env *e, void *va, size_t len, int perm) {
+int vma_new(env_t *e, void *va, size_t len, int perm) {
 
     return 0;
 }
 
-int vma_unmap(struct env *e, void *va, size_t len) {
+int vma_unmap(env_t *e, void *va, size_t len) {
 
     return 0;
 }
 
-vma_t *vma_lookup(struct env *e, void *va) {
+vma_t *vma_lookup(env_t *e, void *va) {
 
     return 0;
 }
 
-void vma_dump_all(struct env *e) {
+void vma_dump_all(env_t *e) {
 }
