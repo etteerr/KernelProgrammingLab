8eebce731cdb413a78503dfe236e06972996e992
etteerr <e.j.diepgrond@gmail.com>
2017-10-07 13:15:14 +0200

    Set timeslice back to 1ms

--- kern/sched.h
+++ kern/sched.h
@@ -6,7 +6,7 @@
 # error "This is a JOS kernel header; user programs should not #include it"
 #endif
 
-#define MAX_TIME_SLICE 1000000000 /* 1ms on 1GHz CPU */
+#define MAX_TIME_SLICE 1000000 /* 1ms on 1GHz CPU */
 
 /* This function does not return. */
 void sched_yield(void) __attribute__((noreturn));
